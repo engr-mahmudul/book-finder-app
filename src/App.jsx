57d43c7628bb4da17d3e4e1c5749fa5f0fe81c44
Mahmudul Hasan <dev.mahmudulhasan@gmail.com>
2024-01-18 07:28:44 +0200

    All design added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "./components/Footer";
+import MainContent from "./components/MainContent";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <body className="relative font-[Manrope] before:fixed before:left-0 before:top-0 before:-z-10 before:h-[435px] before:w-full before:rounded-bl-3xl before:bg-[#EAE6D7] max-md:px-4 lg:text-lg before:lg:rounded-bl-[79px]">
+      <Navbar />
+      <MainContent />
+      <Footer />
+    </body>
+  );
+}
